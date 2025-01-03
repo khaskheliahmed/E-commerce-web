2c4aeef725839e5f14678affddbe68e31de5aab1
khaskheliahmed <ahmadkhaskheli91@gmail.com>
2025-01-03 23:56:21 +0500

    navbar_update

--- src/Compounts/Header.jsx
+++ src/Compounts/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Logo from './Logo'
-import { FaSearch } from "react-icons/fa";
+import { ImSearch } from "react-icons/im";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IoCart } from "react-icons/io5";
 
 const Header = () => {
   return (
@@ -10,15 +12,34 @@ const Header = () => {
                 <div className='' >
                        <Logo w={10} h={50} />
                 </div>
-                <div>
-                    <input type="text" placeholder='seach product here...' />
-                    <div>
-                    <FaSearch />
+
+                {/* //Search_panel */}
+                <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2  ' >
+                    <input type="text" placeholder='seach product here...' className='w-full outline-none   ' />
+                    <div className='text-lg min-w-[50Px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
+                    <ImSearch />
                     </div>
                 </div>
-                <div>
-                    user icons and cards
+                {/* //user_panel */}
+                <div className='flex item-cneter gap-7 '>
+                <div className='text-3xl cursor-pointer '>
+                <FaRegCircleUser />
+                
                 </div>
+                <div className='text-3xl relative'>
+                <span><IoCart /></span>
+                <div className='bg-red-600 absolute -top-2 -right-2 text-white w-5 p-1 flex items-center justify-center h-5 rounded-full '>
+                 <p className='text-xl'>0</p>
+                 </div>
+                </div>
+                <button className='px-3 bg-red-600 py-1 rounded-full  text-white hover:bg-red-700'>login</button>
+                </div>
+
+                
+
+                
+
+
             </div>
         </header>
     </>
